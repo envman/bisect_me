cce8f28154071789a33a8b101cd11dc6bae2cf33
Robert Gill <envman@gmail.com>
2018-07-08 16:48:30 +0100

    more logging: broke it

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ module.exports = function add (a, b) {
   console.log(`Adding ${a} and ${b}`)
   let result =  a + b
 
-  console.log('result:', result)
-  return result
+  return console.log(`The result is ${result}`)
 }
